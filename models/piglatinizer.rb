4334bb714d207fd5e7052adce86fdc3c1ba2c531
John Werenski <jtwerenski@gmail.com>
2018-12-08 20:54:20 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLantinizer 
+  attr_reader :text 
+  
+  def initialize(text)
+    @text = text.downcase
+  end 
+  
+  def converter
+    #take string make array of each word. 
+    #each word find first cononant or group of consonats
+    #push cluter to end of word
+    # then add "ay" to each word
+    arr = text.gsub(/\s+/, " ").split(" ")
+    arr.collect do |word|
+      #remove first consonant group, and put it on the end 
+      #add ay on the end.
+  end 
