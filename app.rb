190c53375e493ffd2ebc6d964d5e4e6c1d05e2e7
John Werenski <jtwerenski@gmail.com>
2018-12-08 21:39:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/converted' do 
+    @converted_text = PigLatinizer.new(params[:user_text])
+    erb :converted
+  end 
+  
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLantinizer 
+  attr_reader :text 
+  
+  def initialize(text)
+    @text = text.downcase
+  end 
+  
+  def converter
+    #take string make array of each word. 
+    #each word find first cononant or group of consonats
+    #push cluter to end of word
+    # then add "ay" to each word
+    arr = text.gsub(/\s+/, " ").split(" ")
+    arr.each do |word|
+      word.match(/[^aeiou]/)
+      #remove first consonant group, and put it on the end 
+      #add ay on the end.
+    end 
+  end 
+end 
